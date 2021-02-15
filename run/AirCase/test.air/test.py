f584602c5f85a20971d3151c2f61507749766ed8
qvchuang <qvchuang@126.com>
2021-02-15 18:45:44 +0800

    submit

---
+++ run/AirCase/test.air/test.py
@@ -0,0 +1,13 @@
+# -*- encoding=utf8 -*-
+__author__ = "quchuang"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+
+
+
+coordinate1 = (739, 1103)
+coordinate2 = (336, 1108)
+swipe(coordinate2,coordinate1)
